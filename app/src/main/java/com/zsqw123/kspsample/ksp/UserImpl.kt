323fecae9fa8c71ef58962aacba347ddef7dc394
damyjy <zsqw123@vip.qq.com>
2021-11-06 17:29:05 +0800

    update: 1.78

--- app/src/main/java/com/zsqw123/kspsample/ksp/UserImpl.kt
+++ app/src/main/java/com/zsqw123/kspsample/ksp/UserImpl.kt
@@ -1,5 +1,6 @@
 package com.zsqw123.kspsample.ksp
 
+import com.zsqw123.inject.CatInject
 import com.zsqw123.kspsample.IUser
 
 /**
@@ -7,17 +8,20 @@ import com.zsqw123.kspsample.IUser
  * Create by damyjy
  * Date 2021/8/8 16:45
  */
-class UserImpl : IUser {
+@CatInject
+class UserImpadsasdl : IUser {
     override fun getName(): String = "my-injected-user"
     override fun getAge(): Int = 1
 }
 
+@CatInject
 class UserImpl2 : IUser {
     override fun getName(): String = "my-injected-user2"
-    override fun getAge(): Int = 2
+    override fun getAge(): Int = 6666
 }
 
-class UserImpl3 : IUser {
-    override fun getName(): String = "my-injected3"
+@CatInject
+class UserImplsdadsa3 : IUser {
+    override fun getName(): String = "my-injected3333"
     override fun getAge(): Int = 3
 }
